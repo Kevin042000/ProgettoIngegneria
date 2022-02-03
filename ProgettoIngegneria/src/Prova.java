1b24ed22e83e0e96cbce26fdd734ec1903e3b49d
kevin <kevin.renda@studio.unibo.it>
2022-02-03 18:39:05 +0100

    funziona?

--- ProgettoIngegneria/src/Prova.java
+++ ProgettoIngegneria/src/Prova.java
@@ -2,7 +2,7 @@ public class Prova {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Funziona!!!");
+		System.out.println("Funziona!!!!");
 		System.out.println("no");
 		System.out.println("si");
 	}
